fbf7556eef555ac8f63bc73156294a148ea68dfb
menamek <free2menna@gmail.com>
2016-02-28 23:20:07 -0600

    W1D6

---
+++ W1D5/assert.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int x=9;
+	int y=8;
+
+	x==(y+1)? printf("first\n"):
+	x>y ? printf("second\n") :
+	x<y ? printf("third\n");:
+}
---
+++ W1D5/checkdef.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+#define bufsize 1024
+#define tablesize bufsize/4
+#undef bufsize
+#define bufsize 512
+
+prinf("%d\n", tablesize);
+
+		}
